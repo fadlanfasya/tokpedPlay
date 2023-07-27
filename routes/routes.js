12b0b99b4f72545792b1ff0fca389cb2d17958b1
fadlanfasya <fasyafadlan7@gmail.com>
2023-07-27 15:40:40 +0700

    add new route

--- routes/routes.js
+++ routes/routes.js
@@ -4,11 +4,13 @@ const videoController = require('../controllers/videoController.js');
 const productController = require('../controllers/productController.js');
 const commentController = require('../controllers/commentController.js');
 
+router.post('/videos', videoController.postData) 
 router.get('/videos', videoController.getAll);
 router.get('/videos/:id', videoController.getId);
 
+router.post('/videos/:id/addproduct', productController.postData);
 router.get('/products', productController.getAll);
 
-router.post('/videos/:id/comment', commentController.postData) 
+router.post('/videos/:id/addcomment', commentController.postData) 
 
 module.exports = router;
