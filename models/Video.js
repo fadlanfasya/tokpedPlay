c8708c97778801d4a0bf7c7931c8edf0acaf1834
fadlanfasya <fasyafadlan7@gmail.com>
2023-07-27 15:39:54 +0700

    add method post

--- models/Video.js
+++ models/Video.js
@@ -3,10 +3,10 @@ const Product = require('./Product.js');
 const Comment = require('./Comment.js');
 
 const videoSchema = new mongoose.Schema({
-    productId: {
+    productId: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: Product
-    },
+    }],
     commentId : {
         type: mongoose.Schema.Types.ObjectId,
         ref: Comment,
@@ -30,7 +30,8 @@ const videoSchema = new mongoose.Schema({
     },
     views: {
         required: true,
-        type: Number
+        type: Number,
+        default: 0
     }
 })
 
